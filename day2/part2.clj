19a31bc43a506264f37184c9c0378a0f67e945e1
Yotam Lender <yotam.lender@nanit.com>
2020-12-07 10:20:06 +0200

    day 2 part 2

--- day2/part2.clj
+++ day2/part2.clj
@@ -1,20 +1,13 @@
 (def input (-> "input.txt" slurp (clojure.string/split #"\n")))
 
-(defn- in-pos? 
-  [line letter pos]
-  (= letter (nth line pos)))
-
 (defn- valid? [line]
   (let [[_ pos1 pos2 letter password] (re-matches #"^(\d+)-(\d+) (.): (.+)$" line)
         pos1 (dec (Integer. pos1))
         pos2 (dec (Integer. pos2)) 
         password (map str password)
-        in-pos? (partial in-pos? password letter)] 
-    (->> [pos1 pos2]
-         (filter in-pos?)
-         count
-         dec
-         zero?)))
+        in-pos1 (= letter (nth line pos1))
+        in-pos2 (= letter (nth line pos2))]
+    (not= in-pos1 in-pos2))
 
 (defn solve [input]
   (->> input
